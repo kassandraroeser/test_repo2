57fb6e6fea666de3eed4e47eade1f6f5aa5c7d1c
Kassandra Roeser <kroeser@stanford.edu>
2024-10-08 23:45:08 -0700

    adding scatterplot

--- code/test_script.R
+++ code/test_script.R
@@ -2,3 +2,11 @@
 # bing bong
 # october 9th, 2024
 
+# Load the tidyverse library
+library(tidyverse)
+
+# Step 3: Adding the scatterplot
+# Create a scatterplot with mpg dataset
+ggplot(mpg, aes(x = displ, y = hwy, color = drv)) +
+  geom_point()
+
