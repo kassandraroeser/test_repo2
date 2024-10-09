b74e1ab6b68ba3aebdcebf6558ab3970fb712dc1
Kassandra Roeser <kroeser@stanford.edu>
2024-10-08 23:51:19 -0700

    final scatterplot

--- code/test_script.R
+++ code/test_script.R
@@ -10,3 +10,11 @@ library(tidyverse)
 ggplot(mpg, aes(x = displ, y = hwy, color = drv)) +
   geom_point()
 
+# Add labels and change theme
+ggplot(mpg, aes(x = displ, y = hwy, color = drv)) +
+  geom_point() +
+  labs(x = "Engine Displacement (L)", 
+       y = "Highway MPG",
+       title = "Highway MPG vs Engine Displacement",
+       color = "Drive Type") +
+  theme_minimal()
