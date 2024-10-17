bb87c1b4fc46891ab665657ac37c5f02f2c3957b
Kassandra Roeser <kroeser@stanford.edu>
2024-10-16 20:29:04 -0700

    pushing test

---
+++ pushingtest.R
@@ -0,0 +1,6 @@
+# i'm working within my test repo2, which is an individual
+# repo with no collaborators. i've cloned the shared
+# repo as a subfolder of my individual git repo instead of 
+# creating a new git version control project
+# i'm now testing to see if i can push changes to the 
+# shared repo
